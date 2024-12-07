fc4ce43e9fd644407e31316c78379a272d93f9e5
Philippe Desmarais <philippe.desmarais0trash@gmail.com>
2024-12-07 16:14:41 -0500

    feat: add google drive file picker service
    This allows us to grant access to specific files to our application, instead of granting global access. That'll help us get around both the google blocking issue, and the kdbx file duplication issue when using limited file scope
    
    The UI elements wrapping around this will be added in a later commit

---
+++ lib/src/FilePicker/IFilePicker.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace FilePicker
+{
+    public interface IFilePicker
+    {
+        Task<FilePick> SelectFile(FilePickerOptions options, CancellationToken cancellationToken);
+    }
+
+
+    public class FilePick
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
