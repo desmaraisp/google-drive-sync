a7e299250806644787e1c755f0a44cccf1ba03ec
Philippe Desmarais <philippe.desmarais0trash@gmail.com>
2024-12-07 16:11:24 -0500

    feat: add build-time configuration accessor
    This will serve to replace the GenClientId process by using commandLine args to pass values to be inlined into the plugin

---
+++ src/OptionsAccessor.cs
@@ -0,0 +1,17 @@
+﻿using CompileTimeConfigPublicMembers;
+
+namespace KPSyncForDrive
+{
+    [GenerateConfigAccessor(typeof(PluginStaticConfiguration))]
+    public partial class OptionsAccessor
+    {
+    }
+
+    public class PluginStaticConfiguration
+    {
+        public string ClientId { get; set; }
+        public string PublicClientSecret { get; set; }
+        public string DriveFilePickerPublicApiKey { get; set; }
+        public string GoogleDrivePickerAppId { get; set; }
+    }
+}
